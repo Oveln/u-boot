30c6896a944603a54ad6f8d50273fe0766593869
Oveln <oveln@outlook.com>
2026-02-25 18:18:39 +0800

    jh7110 embassy_preempt支持，双串口输出

--- board/starfive/visionfive2/spl.c
+++ board/starfive/visionfive2/spl.c
@@ -86,8 +86,121 @@ u32 spl_boot_device(void)
 	}
 }
 
+#define SYS_CRG_BASE		0x13020000
+#define CLK_UART1_APB_OFFSET	0x24c
+#define CLK_UART1_CORE_OFFSET	0x250
+#define CLK_RSTN_3_OFFSET	0x300
+
+/* UART1 registers - reg-shift=2 means 4-byte spacing */
+#define UART1_BASE		0x10010000
+#define UART_RBR		(0x00 << 2)	/* Receive Buffer Register */
+#define UART_THR		(0x00 << 2)	/* Transmit Holding Register */
+#define UART_IER		(0x01 << 2)	/* Interrupt Enable Register */
+#define UART_FCR		(0x02 << 2)	/* FIFO Control Register */
+#define UART_LCR		(0x03 << 2)	/* Line Control Register */
+#define UART_MCR		(0x04 << 2)	/* Modem Control Register */
+#define UART_LSR		(0x05 << 2)	/* Line Status Register */
+#define UART_DLL		(0x00 << 2)	/* Divisor Latch Low */
+#define UART_DLM		(0x01 << 2)	/* Divisor Latch High */
+
+#define UART_LCR_DLAB		0x80	/* Divisor Latch Access Bit */
+#define UART_LCR_8N1		0x03	/* 8 data, 1 stop, no parity */
+#define UART_LSR_TEMT		0x40	/* Transmitter empty */
+#define UART_LSR_THRE		0x20	/* Transmit-hold-register empty */
+#define UART_FCR_ENABLE		0x01	/* Enable FIFO */
+
+/* Oscillator frequency for UART */
+#define UART_CLOCK		24000000	/* 24 MHz oscillator */
+#define UART_BAUDRATE		115200
+
+static void uart1_early_puts(const char *s)
+{
+	volatile u8 *uart = (volatile u8 *)UART1_BASE;
+
+	/* Wait for transmitter to be ready before sending */
+	while (!(readb(uart + UART_LSR) & UART_LSR_THRE))
+		;
+
+	while (*s) {
+		/* Wait for transmit holding register to be empty */
+		while (!(readb(uart + UART_LSR) & UART_LSR_THRE))
+			;
+
+		/* Write character */
+		writeb(*s++, uart + UART_THR);
+	}
+
+	/* Wait for transmitter to be completely empty before returning */
+	while (!(readb(uart + UART_LSR) & UART_LSR_TEMT))
+		;
+}
+
+static void uart1_hw_init(void)
+{
+	volatile u8 *uart = (volatile u8 *)UART1_BASE;
+	u32 div;
+	volatile int i;
+
+	/* Calculate divisor for 115200 baud */
+	div = UART_CLOCK / (16 * UART_BAUDRATE);
+
+	/* Disable interrupts first */
+	writeb(0x00, uart + UART_IER);
+
+	/* Set DLAB to access divisor latches */
+	writeb(UART_LCR_DLAB, uart + UART_LCR);
+
+	/* Set divisor */
+	writeb(div & 0xff, uart + UART_DLL);
+	writeb((div >> 8) & 0xff, uart + UART_DLM);
+
+	/* Clear DLAB and set 8N1 mode */
+	writeb(UART_LCR_8N1, uart + UART_LCR);
+
+	/* Enable FIFO and reset TX/RX FIFO */
+	writeb(UART_FCR_ENABLE | 0x06, uart + UART_FCR);
+
+	/* Set Modem Control Register */
+	writeb(0x00, uart + UART_MCR);
+
+	/* Small delay to let UART stabilize */
+	for (i = 0; i < 1000; i++)
+		__asm__ volatile ("nop");
+}
+
+void uart1_init(void)
+{
+	/* Enable UART1 clock */
+	setbits_le32(SYS_CRG_BASE + CLK_UART1_APB_OFFSET, BIT(31));
+	setbits_le32(SYS_CRG_BASE + CLK_UART1_CORE_OFFSET, BIT(31));
+	clrsetbits_le32(SYS_CRG_BASE + CLK_RSTN_3_OFFSET, BIT(21) | BIT(22), 0);
+
+	/*uart1 tx*/
+	SYS_IOMUX_DOEN(43, LOW);
+	SYS_IOMUX_DOUT(43, 0x44);
+	SYS_IOMUX_SET_DS(43, 3);
+	/*uart1 rx*/
+	SYS_IOMUX_DOEN(42, HIGH);
+	SYS_IOMUX_DIN(42, 55);
+
+	/* Initialize UART hardware */
+	uart1_hw_init();
+}
+
+static void uart1_test(void)
+{
+	uart1_early_puts("\r\n");
+	uart1_early_puts("UART1 SPL Test Start\r\n");
+	uart1_early_puts("Test Complete\r\n");
+}
+
 void board_init_f(ulong dummy)
 {
+	uart1_init();
+
+	/* Test UART1 before DM initialization */
+	uart1_test();
+
 	int ret;
 
 	ret = spl_early_init();
